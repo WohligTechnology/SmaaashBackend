1aab6eea2019712a60d06423a9cc00a4a439fc57
root <root@mean-15dc64e1.c.bitnami-builder.internal>
2017-03-21 14:59:52 +0000

    mm

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-var adminurl = "http://146.148.109.185:94/";
+var adminurl = "http://104.154.79.188:82/";
 // var adminurl = "http://localhost:1337/";
 var uploadurl = adminurl + "upload/";
 var imgpath = uploadurl + "readFile";
